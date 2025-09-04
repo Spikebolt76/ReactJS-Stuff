37c322c2a33fa509973eeaa2a517e24374b0affe
Spikebolt76 <jinesh.s745@gmail.com>
2025-09-04 12:34:18 +0530

    okkk

---
+++ 03_Timer_useEffect_useState/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+export default function Timer() {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(prev => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <h1>⏱ Timer</h1>
+      <p>Elapsed time: {seconds} seconds</p>
+    </div>
+  );
+}
+
